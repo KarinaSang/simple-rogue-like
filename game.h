dcd6ae05064aac9a04bf6f07bc7b8511b18feb41
sherry <ycsang@edu.uwaterloo.ca>
2019-07-24 12:14:48 -0400

    updated main, game, cell

---
+++ game.h
@@ -0,0 +1,46 @@
+#ifndef GAME_H
+#define GAME_H
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Cell;
+class Player;
+class Enemy;
+
+class Game{
+	vector <vector <Cell>> grid; //current floor
+
+	//could have a text display here
+	
+
+	const int BOARDWIDTH = 79;
+	const int BOARDHEIGHT = 30;
+	const int TOTALENEMY = 20;
+	const int TOTALPOTION = 10;
+	const int TOTALTREASURE = 10;
+
+	int floorCount = 1; //starting with floor one
+	int enemyCount = 0;
+	int potionCount = 0;
+	int treasureCount = 0;
+	bool suitEquipped = false;
+
+	Player *player;
+	vector <Enemy> enemies;
+	
+
+	public:
+	Game(Player *);
+	~Game();
+	void init();
+	void nextFloor(); //go to next floor
+	int calculateScore(); //calculate current score
+
+	friend ostream &operator << (ostream &out, const Game &g); 
+};
+
+
+#endif
