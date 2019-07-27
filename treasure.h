9b7ae2ba57622b3a19b64bad047ccdf82757495b
sherry <ycsang@edu.uwaterloo.ca>
2019-07-27 16:48:51 -0400

    .

--- treasure.h
+++ treasure.h
@@ -5,6 +5,7 @@ class Treasure{
 	int value;
 	bool collectable;
 	bool suit = false;
+	bool compass = false;
 
 	public:
 	Treasure(int value, bool collectable);
@@ -12,11 +13,13 @@ class Treasure{
 	//getter
 	int getValue(){return value;}
 	bool isCollectable(){return collectable;}
-	bool getSuit(){return suit;}
+	bool isSuit(){return suit;}
+	bool isCompass(){return compass;}
 
 	//setter
 	void setCollectable(){collectable = true;}
 	void setSuit(){suit = true;}
+	void setCompass(){compass = true;}
 };
 
 #endif
