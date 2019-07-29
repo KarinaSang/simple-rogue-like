9f70db76c9a8065b946ec5e46feb68a6c4f53c77
sherry <ycsang@edu.uwaterloo.ca>
2019-07-29 12:14:05 -0400

    .

--- merchant.d
+++
@@ -1,4 +0,0 @@
-merchant.o: merchant.cc merchant.h enemy.h characterinfo.h position.h \
- cell.h textdisplay.h player.h potion.h treasure.h basicplayer.h \
- elfdecorator.h playerdecorator.h orcdecorator.h dwarfdecorator.h \
- basicenemy.h
